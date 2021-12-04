fd5a3ab398630d2c90188a370c82208dc5fe441f
yiyun <yiyungent@gmail.com>
2021-12-04 17:39:20 +0800

    fix(src/afdian.server/services/configuretgwebhook.cs): webhookAddress

--- src/Afdian.Server/Services/ConfigureTgWebhook.cs
+++ src/Afdian.Server/Services/ConfigureTgWebhook.cs
@@ -29,7 +29,7 @@ public class ConfigureTgWebhook : IHostedService
         // If you'd like to make sure that the Webhook request comes from Telegram, we recommend
         // using a secret path in the URL, e.g. https://www.example.com/<token>.
         // Since nobody else knows your bot's token, you can be pretty sure it's us.
-        var webhookAddress = @$"{_botConfig.HostAddress}/bot/{_botConfig.BotToken}";
+        var webhookAddress = @$"{_botConfig.HostAddress}/webhook/tgbot/{_botConfig.BotToken}";
         _logger.LogInformation("Setting webhook: {webhookAddress}", webhookAddress);
         await botClient.SetWebhookAsync(
             url: webhookAddress,
